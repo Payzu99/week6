bde0b2432edfe023b564df1a8fa68935a28f29c9
Payzu99 <43179079+Payzu99@users.noreply.github.com>
2018-10-18 00:20:56 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,13 @@
+let t = document.querySelector("#tasks");
+let div = t.querySelectorAll("div");
+
+
+
+for(let i = 0; i<div.length; i++){
+    let but = div[i].querySelector("button");
+    but.addEventListener("click",function(e){
+        div[i].dataset.status = 'done';
+});
+}
+
+
